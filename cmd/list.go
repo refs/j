1fb411b1f9787e3c0b0176c21106768a02ac34c5
A.Unger <zyxancf@gmail.com>
2020-03-23 22:53:52 +0100

    add init logic

--- cmd/list.go
+++ cmd/list.go
@@ -1,26 +1,30 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/spf13/cobra"
 )
 
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "lists the last 7 entries on your journal",
-	// Run: func(cmd *cobra.Command, args []string) {
-	// 	var files []string
-	// 	filepath.Walk(HOME, func(path string, info os.FileInfo, err error) error {
-	// 		if err != nil {
-	// 			return err
-	// 		}
-	// 		if !info.IsDir() {
-	// 			files = append(files, info.Name())
-	// 		}
-	// 		return nil
-	// 	})
+	Run: func(cmd *cobra.Command, args []string) {
+		var files []string
+		filepath.Walk(jrnl.Config.Home, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if !info.IsDir() {
+				files = append(files, info.Name())
+			}
+			return nil
+		})
 
-	// 	for _, v := range files {
-	// 		fmt.Printf("- %v\n", v)
-	// 	}
-	// },
+		for _, v := range files {
+			fmt.Printf("- %v\n", v)
+		}
+	},
 }
--- cmd/root.go
+++ cmd/root.go
@@ -12,37 +12,41 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var jrnl *journal.J
+
+func init() {
+	// prefill the file with template.
+	tmpl, err := template.New("header").Parse("date:\t{{.Date}}\n------\n\n\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO use functional options to initialize journal.
+	jrnl = &journal.J{
+		Config: &config.Config{
+			Editor: "vim",
+			// $HOME/.j_entries
+			Home: fmt.Sprintf("%v/%v", os.Getenv("HOME"), ".j_entries"),
+			Format: &config.Format{
+				// YYYY-MM-DD
+				Date:     time.Now().Format("2006-01-02"),
+				Template: tmpl,
+			},
+		},
+	}
+
+	// TODO move this to J's methodset?
+	jrnl.Config.FileName = fmt.Sprintf("%v/%v", jrnl.Config.Home, jrnl.Config.Format.Date)
+
+	// ensure there is a J home dir.
+	initHome(jrnl)
+}
+
 var rootCmd = &cobra.Command{
 	Use:   "j",
 	Short: "j is a zero config journaling tool.",
 	Long:  `j should help you be more organized and hopefully remember more things over time.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		var err error
-
-		// prefill the file with template.
-		tmpl, err := template.New("header").Parse("date:\t{{.Date}}\n------\n\n\n")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// TODO use functional options to initialize journal.
-		jrnl := &journal.J{
-			Config: &config.Config{
-				Editor: "vim",
-				// $HOME/.j_entries
-				Home: fmt.Sprintf("%v/%v", os.Getenv("HOME"), ".j_entries"),
-				Format: &config.Format{
-					// YYYY-MM-DD
-					Date:     time.Now().Format("2006-01-02"),
-					Template: tmpl,
-				},
-			},
-		}
-
-		initHome(jrnl)
-
-		// TODO move this to J's methodset?
-		jrnl.Config.FileName = fmt.Sprintf("%v/%v", jrnl.Config.Home, jrnl.Config.Format.Date)
 		if err := jrnl.Open(); err != nil {
 			log.Fatal(err)
 		}
