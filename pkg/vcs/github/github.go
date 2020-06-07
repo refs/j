36cd7d97f17f756b9c2a906abf0809e0ec5384b1
A.Unger <zyxancf@gmail.com>
2020-06-07 18:18:15 +0200

    use init pattern to load oauth token from environment

--- pkg/vcs/github/github.go
+++ pkg/vcs/github/github.go
@@ -1,5 +1,16 @@
 package github
 
+import (
+	"fmt"
+	"os"
+)
+
+func init() {
+	// load J_OAUTH_TOKEN from env
+	token := os.Getenv("J_OAUTH_TOKEN")
+	fmt.Printf("token read from environment:\t%v\n", token)
+}
+
 // VCS provides a github implementation of vcs.Versioner.
 type VCS struct{}
 
